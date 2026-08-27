b2595460a8d694b52618c61e24d5666212295b07
Aluno <Aluno@fatecdiadema.com>
2026-08-27 10:59:10 -0300

    Update

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,6 @@
-function trocarVideo(video){
+function trocarVideo(video) {
     var player = document.getElementById('playerVideo');
+
     player.src = '/video/' + video;
-    document.getElementById('nomeVideo').innerHTML = video;
- 
     player.play();
 }
